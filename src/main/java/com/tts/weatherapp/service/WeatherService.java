7388ae4964c89542c893d6841bde9d9d0d8c2939
Samber9891 <Slem0119@gmail.com>
2020-11-08 17:15:12 -0500

    added try/catch function and zipcode list methods

--- src/main/java/com/tts/weatherapp/service/WeatherService.java
+++ src/main/java/com/tts/weatherapp/service/WeatherService.java
@@ -1,24 +1,66 @@
 package com.tts.weatherapp.service;
 
-import com.tts.weatherapp.models.Response;
+import java.util.ArrayList;
+import java.util.List;
 
+import com.tts.weatherapp.models.Request;
+import com.tts.weatherapp.models.Response;
+import com.tts.weatherapp.models.ZipCode;
+import com.tts.weatherapp.models.ZipCodeDisplay;
+import com.tts.weatherapp.repositories.ZipCodeRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 @Service
-public class WeatherService {
-    
-    @Value("{$api_key}")
-    private String apiKey; 
+public class WeatherService<ZipCode> {
 
+    @Value("${api_key}")
+    private String apiKey;
 
-    // Make a call to openweater api, with dynamic zipcode and apikey
+    @Autowired
+    ZipCodeRepository zipCodeRepository; 
+
+    // Make a call to openweather api, with dynamic zipcode and apikey
     public Response getForecast(String zipCode){
+
         String url="http://api.openweathermap.org/data/2.5/weather?zip=" + zipCode + "&appid=" + apiKey;
         RestTemplate restTemplate= new RestTemplate(); 
+        
         // Make a call to "URL"
         // Shove the response in a response "object"
-        return restTemplate.getForObject(url, Response.class); 
+        
+        try{
+            return restTemplate.getForObject(url, Response.class);  
+        }
+        catch (HttpClientErrorException exception) {
+            Response response = new Response(); 
+            response.setName("ERROR"); 
+            return response; 
+        }
+
+        public List<ZipCodeDisplay zipCodeList(List<ZipCode> zipCodes) {
+           return Request.getZipCode()); 
+
+        }
+        
+      public List<ZipCodeDisplay> findAll() {
+          List<ZipCode> zipCodes = zipCodeRepository.findAllById();
+          return zipCodeList(zipCodes); 
+      }
+
+        
     }
-}
+
+
+       
+
+
+
+    
+
+    
